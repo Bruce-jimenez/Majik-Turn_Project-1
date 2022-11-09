f6858104d16c87757fae0a132318174451bac4d2
Bruce-jimenez <jimenezbruce2022@gmail.com>
2022-11-09 16:33:43 -0500

    Connection html - js

---
+++ caster.js
@@ -0,0 +1,66 @@
+const arena = () => {
+
+
+const fightStart = () => {
+    const electricSpell = document.querySelector(".Electricity");
+    const voidSpaceSpell = document.querySelector(".Void/Space");
+    const waterSpell = document.querySelector(".Water");
+    const fireSpell = document.querySelector(".Fire");
+    const earthSpell = document.querySelector(".Earth");
+    const playersChoice = [electricSpell, voidSpaceSpell, waterSpell, fireSpell, earthSpell];
+    const golemChoice = ["Electricity", "Void/Space", "Water", "Fire", "Earth"]
+
+
+    playersChoice.forEach(choice => {
+        choice.addEventListener('click',function()){
+
+                    
+
+
+
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
